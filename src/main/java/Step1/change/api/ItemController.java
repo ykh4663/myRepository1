a6d987c76c22059b343ef7adfe6e312e23d64538
Kyungho Yeom <superho7@gmail.com>
2024-11-06 12:10:15 +0900

    second commit

--- src/main/java/Step1/change/api/ItemController.java
+++ src/main/java/Step1/change/api/ItemController.java
@@ -1,12 +1,23 @@
 package Step1.change.api;
 
+import Step1.change.domain.item.Item;
 import Step1.change.service.ItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService itemService;
+
+    @PostMapping("api/members")
+    public void saveItems(@RequestBody @Valid Item item){
+        return;
+    }
+
 
 }
